4428b2c6b6db9fddc342e3e8b6815a323f80b082
Jaouad Sarouti <jaouadsarti@gmail.com>
2026-06-15 07:50:55 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+console.log(add(1, 2));
+console.log(subtract(1, 2));
+console.log(multiply(1, 2));
+console.log(divide(1, 2));
